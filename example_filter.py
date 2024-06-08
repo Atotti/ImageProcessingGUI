382fde85e1d05c934a067da85b93107ccd2a91ff
Ayuto Tsutsumi <56724676+Atotti@users.noreply.github.com>
2024-06-08 14:08:15 +0900

    fix

--- example_filter.py
+++ example_filter.py
@@ -1,12 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
-from filters import gaussian, median, bilateral, sobel, low_pass_filter
+from filters import gaussian, median, bilateral, sobel, low_pass_filter, high_pass_filter
 
 img_path = 'image/IMG_2429.JPG'
 method = low_pass_filter
 
 # 画像の読み込み（カラー）
-if method is low_pass_filter:
+if method is low_pass_filter or method is high_pass_filter:
     image = cv2.imread(img_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
